3fb265ae2221c2091a4a462d4b59f4779fb25af9
Elizabeth Rogers <beth@elizabeth-rogers.com>
2016-01-25 17:07:12 -0600

    bug fix

--- wordpress-boilerplate-source/content/content.php
+++ wordpress-boilerplate-source/content/content.php
@@ -1,9 +1,9 @@
 <article role="article" id="post-<?php the_ID(); ?>" <?php post_class( 'post' ); ?>>
     <header>
         <?php elr_post_title(); ?>
-        <?php elr_post_meta( get_the_ID ); ?>
+        <?php elr_post_meta( get_the_ID() ); ?>
     </header>
     <?php elr_post_thumbnail(); ?>
-    <div><?php elr_post_content( get_the_ID ); ?></div>
+    <div><?php elr_post_content( get_the_ID() ); ?></div>
     <footer><?php elr_edit_link(); ?></footer>
 </article>
