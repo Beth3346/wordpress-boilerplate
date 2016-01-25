029406ae1b23cc423d2015ac9e1882d68e2e4688
Elizabeth Rogers <beth@elizabeth-rogers.com>
2016-01-25 16:01:40 -0600

    removed wrapper div

--- wordpress-boilerplate-source/footer.php
+++ wordpress-boilerplate-source/footer.php
@@ -1,28 +1,26 @@
-        <footer class="main-footer elr-container-full">
-            <div class="elr-row">
-                <div class="elr-col-full">
-                    <nav class="footer-nav" role="navigation">
-                        <?php wp_nav_menu(
-                            array(
-                                'theme_location' => 'footer-nav',
-                                'fallback_cb' => 'default_footer_nav',
-                                'container'  => 'footer-nav-wrapper',
-                                'menu_id' => 'footer-menu',
-                                'menu_class' => 'footer-menu elr-inline-list elr-text-center'
-                            )
-                        ); ?>
-                    </nav>
-                </div>
+    <footer class="main-footer elr-container-full">
+        <div class="elr-row">
+            <div class="elr-col-full">
+                <nav class="footer-nav" role="navigation">
+                    <?php wp_nav_menu(
+                        array(
+                            'theme_location' => 'footer-nav',
+                            'fallback_cb' => 'default_footer_nav',
+                            'container'  => 'footer-nav-wrapper',
+                            'menu_id' => 'footer-menu',
+                            'menu_class' => 'footer-menu elr-inline-list elr-text-center'
+                        )
+                    ); ?>
+                </nav>
             </div>
-        </footer>
-        <div class="copyright elr-container-full elr-text-center">
-            <small>
-                <?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?> All Rights Reserved.
-            </small>
-            <!-- wp_footer -->
-            <?php wp_footer(); ?>
         </div>
-    <!--end wrapper-->
+    </footer>
+    <div class="copyright elr-container-full elr-text-center">
+        <small>
+            <?php bloginfo(); ?> &copy; <?php echo date( 'Y' ); ?> All Rights Reserved.
+        </small>
+        <!-- wp_footer -->
+        <?php wp_footer(); ?>
     </div>
 </body>
 </html>
--- wordpress-boilerplate-source/header.php
+++ wordpress-boilerplate-source/header.php
@@ -39,25 +39,24 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div class="wrapper">
-    <header class="main-header elr-container-full" role="banner">
-        <div class="elr-row">
-            <div class="elr-col-third">
-                <!-- add logo background image images/logo.png -->
-                <p class="site-name"><a href="<?php bloginfo( 'url' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            </div>
-            <div class="navigation-holder elr-col-two-thirds">
-                <nav class="main-nav" role="navigation">
-                    <?php wp_nav_menu(
-                        array(
-                            'theme_location' => 'main-nav',
-                            'fallback_cb' => 'default_main_nav',
-                            'container'  => 'main-nav-wrapper',
-                            'menu_id' => 'main-menu',
-                            'menu_class' => 'main-menu elr-inline-list'
-                        )
-                    ); ?>
-                </nav>
-            </div>
+<header class="main-header elr-container-full" role="banner">
+    <div class="elr-row">
+        <div class="elr-col-third">
+            <!-- add logo background image images/logo.png -->
+            <p class="site-name"><a href="<?php bloginfo( 'url' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
         </div>
-    </header>
+        <div class="navigation-holder elr-col-two-thirds">
+            <nav class="main-nav" role="navigation">
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-nav',
+                        'fallback_cb' => 'default_main_nav',
+                        'container'  => 'main-nav-wrapper',
+                        'menu_id' => 'main-menu',
+                        'menu_class' => 'main-menu elr-inline-list'
+                    )
+                ); ?>
+            </nav>
+        </div>
+    </div>
+</header>
