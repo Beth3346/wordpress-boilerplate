ea9c6c21051ea00341f2e908d3aea51469046eca
Elizabeth Rogers <beth3346@gmail.com>
2016-03-09 22:52:24 -0600

    added shortcodes

--- wordpress-boilerplate-source/framework/shortcodes/elr-shortcodes.php
+++ wordpress-boilerplate-source/framework/shortcodes/elr-shortcodes.php
@@ -2,3 +2,5 @@
 
 require_once( 'functions/elr-categories.php' );
 require_once( 'functions/elr-youtube.php' );
+require_once( 'functions/elr-phone.php' );
+require_once( 'functions/elr-email.php' );
--- wordpress-boilerplate-source/framework/shortcodes/functions/elr-youtube.php
+++ wordpress-boilerplate-source/framework/shortcodes/functions/elr-youtube.php
@@ -5,7 +5,7 @@ add_shortcode( 'elr-youtube', function( $atts ) {
         'src' => '',
     ), $atts ) );
 
-    $string =  '<div class="page-video-holder"><div class="elr-video-wrapper">';
+    $string = '<div class="page-video-holder"><div class="elr-video-wrapper">';
     $string .= '<iframe width="560" height="315" src="';
     $string .= $src;
     $string .= '" frameborder="0" allowfullscreen></iframe></div></div>';
