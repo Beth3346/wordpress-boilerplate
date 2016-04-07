aa01f13ba2fb2743a8c904a34d7bfc068fbe731d
Elizabeth Rogers <beth3346@gmail.com>
2016-04-07 01:02:55 -0500

    shortcodes

--- wordpress-boilerplate-source/framework/shortcodes/functions/elr-youtube.php
+++ wordpress-boilerplate-source/framework/shortcodes/functions/elr-youtube.php
@@ -3,10 +3,12 @@
 add_shortcode( 'elr-youtube', function( $atts ) {
     extract( shortcode_atts( array(
         'src' => '',
+        'width' => 560,
+        'height' => 315
     ), $atts ) );
 
     $string = '<div class="page-video-holder"><div class="elr-video-wrapper">';
-    $string .= '<iframe width="560" height="315" src="';
+    $string .= '<iframe width="' . $width . '" height="' . $height . '" src="';
     $string .= $src;
     $string .= '" frameborder="0" allowfullscreen></iframe></div></div>';
 
