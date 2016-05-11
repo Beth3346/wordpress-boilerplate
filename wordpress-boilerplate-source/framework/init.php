64b059be454986e95c89c9ad4567600425535f04
Elizabeth Rogers <beth3346@gmail.com>
2016-05-10 23:06:56 -0500

    updated init

--- wordpress-boilerplate-source/framework/init.php
+++ wordpress-boilerplate-source/framework/init.php
@@ -1,44 +1,11 @@
 <?php
 
-<<<<<<< HEAD
 require 'classes/ELR-CPT-Builder.php';
 require 'classes/ELR-Custom-Fields.php';
 require 'classes/ELR-Framework.php';
-// require 'classes/ELR-Mail.php';
-// require 'classes/ELR-Option.php';
-// require 'classes/ELR-Shortcode.php';
-=======
-// require 'classes/ELR-Admin.php';
-// require 'classes/ELR-Archive.php';
-// require 'classes/ELR-Comment.php';
-// require 'classes/ELR-Content.php';
-require 'classes/ELR-CPT-Builder.php';
-// require 'classes/ELR-CPT.php';
-require 'classes/ELR-Custom-Fields.php';
-require 'classes/ELR-Framework.php';
-// require 'classes/ELR-Helpers.php';
-// require 'classes/ELR-Mail.php';
-// require 'classes/ELR-Navigation.php';
-// require 'classes/ELR-Option.php';
-// require 'classes/ELR-Pagination.php';
-// require 'classes/ELR-Post.php';
-// require 'classes/ELR-Post-Query.php';
-// require 'classes/ELR-Security.php';
-// require 'classes/ELR-Setup.php';
-// require 'classes/ELR-Shortcode.php';
-// require 'classes/ELR-Tax.php';
-// require 'classes/ELR-Time.php';
-// require 'classes/ELR-Validation.php';
->>>>>>> origin/master
-require 'custom-posts/elr-cpt-service.php';
-require 'shortcodes/elr-shortcodes.php';
 
 $framework = new ELR_Framework;
-<<<<<<< HEAD
 // $elr_fields = new ELR_Custom_Fields;
-=======
-$elr_fields = new ELR_Custom_Fields;
->>>>>>> origin/master
 
 // Selects Custom Post Type Templates for single and archive pages
 add_filter('template_include', [$framework, 'custom_template_include']);
@@ -76,7 +43,6 @@ add_filter('excerpt_more', [$framework, 'custom_more']);
 
 add_filter('excerpt_length', [$framework, 'custom_excerpt_length']);
 
-<<<<<<< HEAD
 // $service_fields = [
 //     [
 //         'id' => '_service_name',
@@ -119,47 +85,3 @@ add_filter('excerpt_length', [$framework, 'custom_excerpt_length']);
 // add_action('init', function() use ($service_fields) { elr_register_service_fields($service_fields); }, 12);
 // add_action('save_post', function() use ($service_fields) { elr_save_service_fields($service_fields); }, 12);
 // add_action('add_meta_boxes', function() use ($service_fields) { elr_add_meta_service_box($service_fields); }, 12);
-=======
-$service_fields = array(
-    array(
-        'id' => '_service_name',
-        'label' => 'Service'
-    ),
-    array(
-        'id' => '_service_type',
-        'label' => 'Type',
-        'type' => 'textarea'
-    ),
-    array(
-        'id' => '_service_area',
-        'label' => 'Area',
-        'type' => 'select',
-        'options' => array(
-            'marketing',
-            'service',
-            'technology'
-        )
-    )
-);
-
-function elr_register_service_fields($service_fields) {
-    $elr_fields = new ELR_Custom_Fields;
-    return $elr_fields->meta_fields_register($service_fields);
-}
-
-function elr_save_service_fields($service_fields) {
-    global $post;
-
-    $elr_fields = new ELR_Custom_Fields;
-    return $elr_fields->meta_box_save($post->ID, $service_fields);
-}
-
-function elr_add_meta_service_box($service_fields) {
-    $elr_fields = new ELR_Custom_Fields;
-    return $elr_fields->meta_box_add('service_box', 'Services', 'page', $service_fields);
-}
-
-add_action('init', function() use ($service_fields) { elr_register_service_fields($service_fields); }, 12);
-add_action('save_post', function() use ($service_fields) { elr_save_service_fields($service_fields); }, 12);
-add_action('add_meta_boxes', function() use ($service_fields) { elr_add_meta_service_box($service_fields); }, 12);
->>>>>>> origin/master
